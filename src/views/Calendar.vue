9b7abbe34246f482bde9216764b7601bb79e49a1
yepi-bit <1607562910@qq.com>
2022-09-26 22:22:24 +0800

    x

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="top-box" style="width: 30%;height: 330px;padding:10px;margin: auto">
-    <el-calendar v-model="calendarValue"></el-calendar>
+    <el-calendar v-model="calendarValue" ref="calendar" @click="calendarClick">
+      <template
+          slot="dateCell"
+          slot-scope="{date,data}"
+      >
+        <div :class="calendarData.indexOf(data.day) > -1 ? 'lar-is-selected' : 'lar-no-selected'"
+             @click="holidayUpdate(data,date)"></div>
+      </template>
+    </el-calendar>
+    <el-button @click="skip('today')">今天</el-button>
   </div>
 </template>
 
@@ -9,8 +18,31 @@ export default {
   name: "Calendar",
   data() {
     return {
-      calendarValue:""
+      calendarValue: "",
+      calendarData: [
+        '2022-09-25',
+        '2022-09-26',
+        '2022-09-27'
+      ]
     }
+  },
+  mounted() {
+    // console.log(this.$refs.calendar)
+  },
+  methods: {
+    calendarClick(e) {
+      console.log(e)
+    },
+    skip(val) {
+      if (val === 'today') {
+        this.calendarValue = new Date()
+        console.log(this.calendarValue)
+      }
+    },
+    holidayUpdate(data,date) {
+      console.log(data,date,"1111111111111111111")
+    }
+
   }
 }
 </script>
@@ -20,19 +52,24 @@ export default {
   background-color: #132a46;
   color: #fff;
 }
+
 ::v-deep .el-calendar .el-calendar__title {
   color: #fff;
 }
+
 ::v-deep .el-calendar .el-button {
   background: transparent;
   color: #fff;
 }
+
 ::v-deep .el-calendar .el-calendar-table thead th {
   color: #fff;
 }
+
 ::v-deep .el-calendar .el-calendar-table td {
   border: 1px solid #42556b;
 }
+
 ::v-deep .el-calendar .el-calendar__header {
   border-bottom: 1px solid #42556b;
 }
@@ -48,5 +85,11 @@ export default {
 
 ::v-deep .el-calendar-table thead th {
   padding: 5px 0;
+}
+.lar-is-selected {
+  background-color: rebeccapurple !important;
+}
+.lar-no-selected {
+  background-color: rosybrown !important;
 }
 </style>
