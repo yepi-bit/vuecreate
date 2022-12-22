8f156e3793a4b1142137ca648a604c5464c56ca5
yepi-bit <1607562910@qq.com>
2022-12-23 00:24:43 +0800

    x

--- components.d.ts
+++ components.d.ts
@@ -30,16 +30,10 @@ declare module '@vue/runtime-core' {
     TimeDiff: typeof import('./src/components/timeDiff.vue')['default']
     UploadMe: typeof import('./src/components/UploadMe.vue')['default']
     VanButton: typeof import('vant/es')['Button']
-<<<<<<< HEAD
-=======
-    VanCell: typeof import('vant/es')['Cell']
-    VanCellGroup: typeof import('vant/es')['CellGroup']
->>>>>>> ade09a498f55a4baa41f81588aae5547f4d7ad7b
     VanCheckbox: typeof import('vant/es')['Checkbox']
     VanCheckboxGroup: typeof import('vant/es')['CheckboxGroup']
     VanDivider: typeof import('vant/es')['Divider']
     VanField: typeof import('vant/es')['Field']
-    VanList: typeof import('vant/es')['List']
     VanProgress: typeof import('vant/es')['Progress']
     VanRadio: typeof import('vant/es')['Radio']
     VanRadioGroup: typeof import('vant/es')['RadioGroup']
